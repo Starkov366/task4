b31a623981efdb2b5ed60c3649cb58c60250c6bb
Пользователь <valuznnicartem@gmail.com>
2026-06-10 15:36:24 +0300

    delete init db

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs";
-import pool from "./config/db";
+
 import authRoutes from '../src/modules/auth/routes'
 import usersRoutes from '../src/modules/users/routes'
 const app = express();
@@ -12,11 +11,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 
-async function initDataBase() {
-    const sql = fs.readFileSync("./src/database/schema.sql", "utf8");
-    await pool.query(sql);
-}
 
-initDataBase();
+
+
 
 export default app;
