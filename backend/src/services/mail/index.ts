f748ee8db84c1cd30b00568bc7acfe223ecd260b
Пользователь <valuznnicartem@gmail.com>
2026-06-10 17:28:09 +0300

    fix: send email

--- backend/src/services/mail/index.ts
+++ backend/src/services/mail/index.ts
@@ -1,27 +1,27 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
-    },
-    connectionTimeout: 5000,
-    socketTimeout: 5000,
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(email: string, token: string) {
     const link = `${process.env.BASE_URL}/api/auth/verify/${token}`;
 
-    await transporter.sendMail({
-        from: process.env.EMAIL,
-        to: email,
-        subject: "Verify your account",
-        html: `
-            <h3>Verify account!</h3>
-            <a href="${link}">verify</a>
-        `
-    });
+    console.log("EMAIL START:", email);
+
+    try {
+        const result = await resend.emails.send({
+            from: "onboarding@resend.dev",
+            to: email,
+            subject: "Verify your account",
+            html: `
+                <h3>Verify account!</h3>
+                <a href="${link}">verify</a>
+            `,
+        });
+
+        console.log("EMAIL SENT:", result);
+        return result;
+
+    } catch (err) {
+        console.error("EMAIL ERROR:", err);
+    }
 }
