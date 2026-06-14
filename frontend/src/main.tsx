65619eb159768a80993051f4002b8fa1dec0169e
Пользователь <valuznnicartem@gmail.com>
2026-06-14 12:04:11 +0300

    feat(auth): add login redirect and fix response parsing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
 import { AppRouter } from './router'
+import ProtectedRoute from './router/protectedRoute.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ProtectedRoute>
     <App />
+    </ProtectedRoute>
     <AppRouter/>
+    
   </StrictMode>,
 )
