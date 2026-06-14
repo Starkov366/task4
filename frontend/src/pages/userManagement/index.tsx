57f5ff4a25148c1f138f4bc54c6240f78ecfe2bf
Пользователь <valuznnicartem@gmail.com>
2026-06-14 12:27:52 +0300

    feat(auth): fix last login

--- frontend/src/pages/userManagement/index.tsx
+++ frontend/src/pages/userManagement/index.tsx
@@ -137,7 +137,7 @@ const UserManagement = () => {
                                 </td>
 
                                 <td className={styles.lastSeen}>
-                                    <div>{u.lastLogin}</div>
+                                    <div>{u.last_login}</div>
                                     <div className={styles.bars}>
                                         <span />
                                         <span />
